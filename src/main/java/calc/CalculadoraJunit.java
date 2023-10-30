ccc32dbc3c866b8069aa0088676365f59d3a6141
rafapcmor <rafapcmor@gmail.com>
2023-10-30 17:16:59 -0300

    Calculadora Junit

---
+++ src/main/java/calc/CalculadoraJunit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package calc;
+
+/**
+ *
+ * @author rafaelamoreira
+ */
+public class CalculadoraJunit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
